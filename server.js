4b2a636ed822f28be0af770d2a0f05dcda68ee0d
syyrpp <calebjenkins17@yahoo.com>
2020-03-09 12:32:16 -0500

    server host update

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-perpendicular-69605',
-    user : 'postgres',
-    password : 'Tigerwoods1@',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
